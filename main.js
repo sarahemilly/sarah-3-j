d396eba3b5e9745c7f565ea7a061f2b2e37a44b8
sarahemilly <110680621+sarahemilly@users.noreply.github.com>
2024-06-07 09:49:21 -0300

    07/06

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+const conteudo = document.querySelectorAll(".aba-conteudo");
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
 }
