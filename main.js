e57a7d6e58ddb7c81c4805e265a5ef69eddd1581
sarahemilly <110680621+sarahemilly@users.noreply.github.com>
2024-06-12 22:24:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
+const conteudo = document.querySelectorAll(".aba-conteudo")
+//console.log(botoes);
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-            conteudo[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
