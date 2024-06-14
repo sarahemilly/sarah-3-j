b1a96020d0644efbb86c8d81169aa407edabcf51
sarahemilly <110680621+sarahemilly@users.noreply.github.com>
2024-06-14 09:48:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo")
-const tempo = document.querySelectorAll(".contador")
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const tempo = document.querySelectorAll(".contador");
+ tempo[0].textContent = "ola"
+ const tempoObjetivo1 = new Date(22);
+ const tempoObjetivo2 = new Date(08);
+ const tempoObjetivo3 = new Date(2024);
+ const tempoObjetivo4 = new Date(11);
+ const tempoObjetivo5 = new Date(10);
+ const agora = new Date();
+ tempo[1].textContent = agora
 //console.log(botoes);
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
