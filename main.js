239ad6012328fe0b67dc316aecb00c89775e9612
sarahemilly <110680621+sarahemilly@users.noreply.github.com>
2024-06-20 07:58:08 -0300

    20/06

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
-const tempo = document.querySelectorAll(".contador");
- tempo[0].textContent = "ola"
- const tempoObjetivo1 = new Date(22);
- const tempoObjetivo2 = new Date(08);
- const tempoObjetivo3 = new Date(2024);
- const tempoObjetivo4 = new Date(11);
- const tempoObjetivo5 = new Date(10);
- const agora = new Date();
- tempo[1].textContent = agora
-//console.log(botoes);
-for(let i = 0; 1 < botoes.length ; i++){
+const conteudo = document.querySelectorAll(".aba-conteudo")
+const tempo = document.querySelectorAll(".contador")
+tempo[0].textContent = "hello,tudo bem?"
+const tempoObjetivo1 = new Date(2024,7,13,20);
+const tempoObjetivo2 = new Date(05);
+const tempoObjetivo3 = new Date(2006);
+const tempoObjetivo4 = new Date(10);
+const tempoObjetivo5 = new Date(09);
+const agora = new Date();
+tempo[0].textContent = tempoObjetivo1-agora
+tempo[1].textContent = tempoObjetivo1
+let segundos;
+let minutos;
+let horas;
+let dias;
+segundos= (tempoObjetivo1-agora)/1000
+minutos= segundos/60
+horas= minutos/60
+dias= horas/24
+
+console.log(botoes);
+for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
